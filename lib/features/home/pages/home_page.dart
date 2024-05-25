f70c0e1d47bd055b640eadd3762d003d2e72c947
Victor78970513 <yer29.chok@gmail.com>
2024-05-24 20:43:53 -0400

    tabBar view added

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -1,22 +1,13 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:jayani_power/features/auth/bloc/auth_bloc.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final authBloc = context.watch<AuthBloc>();
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            authBloc.add(OnUserSignOut());
-          },
-          child: Text("salite choco para cambiar de cuenta"),
-        ),
-      ),
-    );
+        body: Center(
+      child: Text("Home"),
+    ));
   }
 }
