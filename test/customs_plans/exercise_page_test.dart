14de55607dcefc78960382615e4b5153c334f5e5
Victor78970513 <yer29.chok@gmail.com>
2024-06-02 02:03:03 -0400

    merge main

--- test/customs_plans/exercise_page_test.dart
+++ test/customs_plans/exercise_page_test.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:jayani_power/features/custom_plans/pages/exercise_page.dart';
 import 'package:jayani_power/features/custom_plans/widgets/exercise_card.dart';
-import 'package:jayani_power/main.dart';
 
 void main() {
   testWidgets('Exercise Page has a title and list of exercises',
